cc82325b5a4c395a919bedb993bc8e9fb58e4d2d
Antonio de la Rosa <santiagodelarosaantonio@gmail.com>
2022-06-14 18:57:53 +0200

    DAO de Review modificado

--- src/main/java/com/starbook/app/dao/ReviewDAO.java
+++ src/main/java/com/starbook/app/dao/ReviewDAO.java
@@ -4,6 +4,7 @@ import com.starbook.app.domain.Mail;
 import com.starbook.app.domain.Review;
 import com.starbook.app.util.DateUtils;
 
+import java.net.Inet4Address;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -73,6 +74,48 @@ public class ReviewDAO {
         }
         st.close();
         return reviews;
+    }
+
+    public Boolean addReview(Review review) throws SQLException{
+        String sql = "INSERT INTO REVIEWS (ISBN, ID_USER, TITLE, DESCRIPTION, STARS) VALUES (?, ?, ?, ?, ?)";
+
+        PreparedStatement st = connection.prepareStatement(sql);
+        st.setString(1, review.getISBN());
+        st.setInt(2, review.getIdUser());
+        st.setString(3, review.getTitle());
+        st.setString(4, review.getDescription());
+        st.setInt(5, review.getStars());
+
+        int rows = st.executeUpdate();
+        st.close();
+        return rows == 1;
+    }
+
+    public Boolean deleteReview(Integer id) throws SQLException{
+        String sql = "DELETE FROM REVIEWS WHERE ID_REVIEW = ?";
+
+        PreparedStatement st = connection.prepareStatement(sql);
+        st.setInt(1, id);
+        int rows = st.executeUpdate();
+        st.close();
+
+        return rows == 1;
+    }
+
+    public Boolean modifyReview(Review review) throws SQLException {
+        String sql = "UPDATE REVIEWS SET TITLE = ?, DESCRIPTION = ?, STARS = ? WHERE ID_REVIEW = ? AND ID_USER = ?";
+
+        PreparedStatement st = connection.prepareStatement(sql);
+        st.setString(1,review.getTitle());
+        st.setString(2,review.getDescription());
+        st.setInt(3,review.getStars());
+        st.setInt(4,review.getIdReview());
+        st.setInt(5,review.getIdUser());
+
+        int rows = st.executeUpdate();
+        st.close();
+
+        return rows == 1;
     }
 
     private Review fromResultSet(ResultSet res) throws SQLException {
