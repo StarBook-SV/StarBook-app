fafb2c3108d36294f7c3eaf2509d50cb07947e90
Holewalker <dani75esp@gmail.com>
2022-06-15 17:17:36 +0200

    Funcionalidad registrar Libros - addBook.jsp - addBookServlet.java - AuthorDAO.java (findByName) - BookAlreadyExistsException.java - AuthorNotFoundException.java

---
+++ src/main/java/com/starbook/app/servlets/AddBookServlet.java
@@ -0,0 +1,64 @@
+package com.starbook.app.servlets;
+
+import com.starbook.app.dao.AuthorDAO;
+import com.starbook.app.dao.BookDAO;
+import com.starbook.app.dao.Database;
+import com.starbook.app.dao.UserDAO;
+import com.starbook.app.domain.Author;
+import com.starbook.app.domain.Book;
+import com.starbook.app.domain.User;
+import com.starbook.app.exception.AuthorNotFoundException;
+import com.starbook.app.exception.BookAlreadyExistsException;
+import com.starbook.app.exception.UsernameAlreadyExistsException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.Optional;
+
+@WebServlet("/addBook")
+public class AddBookServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        Author author;
+        Database database = new Database();
+        AuthorDAO authorDAO = new AuthorDAO(database.getConnection());
+        BookDAO bookDAO = new BookDAO(database.getConnection());
+        Integer idAuthor ;
+        String authorName = req.getParameter("author");
+        String ISBN = req.getParameter("ISBN");
+        Integer idGenre = Integer.valueOf(req.getParameter("genre"));
+        String title = req.getParameter("title");
+        String description = req.getParameter("description");
+        Integer pages = Integer.valueOf(req.getParameter("pages"));
+        try {
+            if (bookDAO.findByISBN(ISBN).isPresent()) throw new BookAlreadyExistsException();
+            author = authorDAO.findByName(authorName).orElseThrow(AuthorNotFoundException::new);
+            idAuthor = author.getId_author();
+            Book book = new Book(ISBN, idAuthor, idGenre, title, description, pages);
+            try {
+                bookDAO.addBook(book);
+                out.println("<div class='alert alert-success' role='alert'>Book added to database</div>");
+            } catch (SQLException sqle) {
+                out.println("<div class='alert alert-danger' role='alert'>An error has occurred</div>");
+            }
+        } catch (SQLException e) {
+            out.println("<div class='alert alert-danger' role='alert'>An error has occurred</div>");
+        } catch (AuthorNotFoundException | BookAlreadyExistsException ee) {
+            out.println("<div class='alert alert-danger' role='alert'>");
+            out.println(ee.getMessage());
+            out.println("</div>");
+        } finally {
+            database.close();
+        }
+    }
+}
+
